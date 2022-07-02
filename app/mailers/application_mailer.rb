4a423c2511b8a05d92ce24549e740dd6366ce441
aarvil <aarvil_git@outlook.com>
2022-07-02 00:28:57 +0000

    fixed sendgrid config for produ

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "aaron.villarreal@gmail.com"
+  default from: "support@ruby-gems-bootcamp-aarvil.herokuapp.com"
   layout "mailer"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { :host => 'https://boxing-lumberjack-63072.herokuapp.com/', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'ruby-gems-bootcamp-aarvil.herokuapp.com/', :protocol => 'https' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
